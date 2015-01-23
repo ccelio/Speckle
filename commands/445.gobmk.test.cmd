4b9a440fc73b09df5b84039bc2bf29f9cf3d1146
Christopher Celio <celio@eecs.berkeley.edu>
2015-01-23 15:32:32 -0800

    riscv-pk can now open more than 64 files, so all of gobmk now works.

--- commands/445.gobmk.test.cmd
+++ commands/445.gobmk.test.cmd
@@ -1,5 +1,5 @@
 --quiet --mode gtp < capture.tst > capture.out 
-#--quiet --mode gtp < connect.tst > connect.out
+--quiet --mode gtp < connect.tst > connect.out
 --quiet --mode gtp < connect_rot.tst > connect_rot.out
 --quiet --mode gtp < connection.tst > connection.out
 --quiet --mode gtp < connection_rot.tst > connection_rot.out
