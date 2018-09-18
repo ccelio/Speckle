afb202131fed1e95648a3afc9b62e3d13cca8370
Cloud User <centos@ip-192-168-0-122.ec2.internal>
2018-09-18 03:55:15 +0000

    run SPEC with different input sizes

---
+++ commands/intrate/520.omnetpp_r.train.cmd
@@ -0,0 +1,1 @@
+-c General -r 0 > omnetpp.General-0.out 2>> omnetpp.General-0.err
---
+++ commands/intspeed/600.perlbench_s.train.cmd
@@ -0,0 +1,5 @@
+-I./lib diffmail.pl 2 550 15 24 23 100 > diffmail.2.550.15.24.23.100.out 2>> diffmail.2.550.15.24.23.100.err
+-I./lib perfect.pl b 3 > perfect.b.3.out 2>> perfect.b.3.err
+-I. -I./lib scrabbl.pl < scrabbl.in > scrabbl.out 2>> scrabbl.err
+-I./lib splitmail.pl 535 13 25 24 1091 1 > splitmail.535.13.25.24.1091.1.out 2>> splitmail.535.13.25.24.1091.1.err
+-I. -I./lib suns.pl > suns.out 2>> suns.err
